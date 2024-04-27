7c0f451f9835593f7bd988514e68ab0f4e141763
tep-vila <vila.sj.bscs@gmail.com>
2024-04-27 11:55:59 +0800

    added the base structure with the img

---
+++ src/js/index.js
@@ -0,0 +1,8 @@
+import "../styles/index.css";
+
+console.log("babyshark dududud");
+
+// https://theskinnypot.com/wp-content/uploads/2021/02/Bistek-Tagalog-Recipe.jpg
+// https://images.deliveryhero.io/image/foodpanda/recipes/nilagang-baboy-recipe-1.jpg
+// https://hips.hearstapps.com/vidthumb/images/pork-adobo-1-jpg-1654712703.jpg?crop=0.657xw:0.876xh;0.175xw,0.0534xh&resize=1200:*
+// https://upload.wikimedia.org/wikipedia/commons/thumb/f/f5/Sinigang_na_Baboy_DSCF4234.jpg/1200px-Sinigang_na_Baboy_DSCF4234.jpg
